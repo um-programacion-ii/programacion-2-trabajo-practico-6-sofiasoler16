8b9fb51a40eee9ab15acac82620bc78330de8503
s.soler <s.soler@alumno.um.edu.ar>
2025-09-11 15:37:44 -0300

    Creadas entidades Producto, Categoria e Inventario

---
+++ microservice-system/data-service/src/main/java/entity/Categoria.java
@@ -0,0 +1,29 @@
+package entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "categoria")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nombre;
+
+    @Column(length = 500)
+    private String descripcion;
+
+    private List<Producto> productos = new ArrayList<>();
+}
