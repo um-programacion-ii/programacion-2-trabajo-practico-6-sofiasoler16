4f11bd474de6db5ecdb1c6ccf54ab8aae6bcc46b
s.soler <s.soler@alumno.um.edu.ar>
2025-09-19 09:32:07 -0300

    Implementados todos los DTO, request y response de producto

---
+++ microservice-system/business-service/src/main/java/businessService/dto/ProductoResponse.java
@@ -0,0 +1,13 @@
+package businessService.dto;
+
+import java.math.BigDecimal;
+
+public class ProductoResponse {
+    private Long id;
+    private String nombre;
+    private String descripcion;
+    private BigDecimal precio;
+    private String categoria;
+    private Integer stock;
+}
+
